c47daaa167bb2eecbf05a628daa20dcb66006b77
shervain <jay_94@live.com>
2017-01-09 12:11:54 -0500

    added vagrant file and the associated scripts

---
+++ _vagrant/vagrant.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='khonsa'
+PROJECTFOLDER='khonsa'
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+# Create project folder, written in 3 single mkdir-statements to make sure this runs everywhere without problems
+sudo mkdir "/var/www"
+sudo mkdir "/var/www/html"
+sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${PROJECTFOLDER}/public"
+    <Directory "/var/www/html/${PROJECTFOLDER}/public">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# remove default apache index.html
+sudo rm "/var/www/html/index.html"
+
+# install git
+sudo apt-get -y install git
+
+# git clone khonsa
+sudo git clone https://github.com/direwolf2494/khonsa.git "/var/www/html/${PROJECTFOLDER}"
+
+# Installation was successful
+echo "Khonsa has been installed successfully!"
